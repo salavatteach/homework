d9962c681f70d1d268c85ea15b64cc02e96348f6
salavatteach <salavat.seit.23981165@gmail.com>
2024-10-13 22:32:36 +0300

    Add files via upload

---
+++ lesson81/main.js
@@ -0,0 +1,11 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for (let i = 0; i < numbers.length; i= i + 2) {
+    console.log(numbers[i]+1);
+}
+
+const colors = ['красный', 'оранжевый', 'желтый', 'зеленый', 'голубой', 'синий', 'фиолетовый']
+
+for (let i = colors.length; i >= 0; i--) {
+    console.log(colors[i]);
+}

62b88ec9713f9637205457a896813e5f52923cd7
salavatteach <salavat.seit.23981165@gmail.com>
2024-10-13 21:20:35 +0300

    Add files via upload

--- lesson76/main.js
+++ lesson76/main.js
@@ -1,11 +1,35 @@
-const firstName = 'John'
-const lastName = 'Smith'
-const age = '25'
-const weight = '68'
+
+const age = 17
+
+if(age >= 18){
+    console.log('Вы совершеннолетний')
+} else{
+    console.log('Вам нет  18')
+}
+
+age >= 18 ? console.log('Вы совершеннолетний') :  console.log('Вам нет  18')
 
 
 
+for(let p = 2; p <= 12; p= p+2){
+    console.log(`Прошел ${p} круг`)
+}
 
 
-console.log(firstName,lastName,age, 'y.o',
-    weight,'kg')
+for(let i = 10; i >= 1 ;i--){
+    console.log(`Прошел ${i} круг`)
+} 
+
+
+let p = 0;
+while (p <= 10) {
+    p = p + 2
+    console.log(`Закончился ${p} круг`)
+}
+
+
+let i = 10;
+while (i >= 1) {
+    i = i-1
+    console.log(`Закончился ${i} круг`)
+}

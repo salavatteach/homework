5595416acd83c73368a88b80e72d77a62d6e68d5
salavatteach <salavat.seit.23981165@gmail.com>
2024-10-16 22:19:39 +0300

    Add files via upload

---
+++ lesson83/main.js
@@ -0,0 +1,61 @@
+const user = {
+    name: 'salavat',
+    age: ' 26 y.o',
+    dateBorn: 23.11
+}
+console.log(user)
+
+const man = {
+    alex: {
+        age: ' 26 y.o',
+        dateBorn: 23.11
+    },
+    sayHello(name) {
+        console.log(`Hello ${name}`)
+    }
+}
+man.sayHello('Sasha')
+
+
+
+const users = [
+    {
+        name: 'max',
+        age: 21,
+        gender: 'male',
+        isAdmin: false
+    },
+     {
+        name: 'paul',
+        age: 21,
+        gender: 'male',
+        isAdmin: false
+    },
+    {
+        name: 'tom',
+        age: 21,
+        gender: 'male',
+        isAdmin: true
+    },
+    {
+        name: 'sasha',
+        age: 21,
+        gender: 'female',
+        isAdmin: false
+    },
+    {
+        name: 'masha',
+        age: 21,
+        gender: 'female',
+        isAdmin: false
+    }
+
+]
+const notAdmin = 0
+for (let i = 0; i < users.length; i++) {
+    if(users[i].isAdmin == false){
+        const resault = notAdmin + 1
+        console.log(resault)
+    }
+}
+

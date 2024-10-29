39cc87d7b8f2b2f0871f7ad8a61e87549831c2fa
salavatteach <salavat.seit.23981165@gmail.com>
2024-10-29 22:23:11 +0300

    Add files via upload

--- website/js/main.js
+++ website/js/main.js
@@ -1,4 +1,7 @@
 (function () {
+
+    // Бургер
+
     document.addEventListener('click', burgerInit)
     function burgerInit(e) {
 
@@ -18,28 +21,91 @@
 
     }
 
+    // Модалка
 
-const btnModal = document.querySelector('.about__img-button')
-const modal = document.querySelector('.modal')
+    const btnModal = document.querySelector('.about__img-button')
+    const modal = document.querySelector('.modal')
+
+    btnModal.addEventListener('click', openModal)
+    modal.addEventListener('click', closeModal)
+
+    function openModal(e) {
+        e.preventDefault()
+        modal.classList.add('modal--opened')
+    }
+
+    function closeModal(e) {
+        e.preventDefault()
+
+        const target = e.target
+        if (target.closest('.modal__cancel') || target.classList.contains('modal')) {
+            modal.classList.remove('modal--opened')
+        }
+    }
+
+    //Табы
+
+    const tabControls = document.querySelector('.programm__tabs-list')
+
+    tabControls.addEventListener('click', toggleTab)
+
+    function toggleTab(e) {
+        const tabControl = e.target.closest('.programm__tabs-btn')
+
+        if (!tabControl) return
+
+        e.preventDefault()
+
+        const tabContentID = tabControl.getAttribute('href')
 
 
 
+        document.querySelector('.tab-content--show').classList.remove('tab-content--show')
+        document.querySelector(tabContentID).classList.add('tab-content--show')
 
-btnModal.addEventListener('click', openModal)
-modal.addEventListener('click', closeModal)  
 
-   function openModal(e) {
-    e.preventDefault()
-    modal.classList.add('modal--opened')
-}
+        document.querySelector('.programm__tabs-btn--active').classList.remove('programm__tabs-btn--active')
+        tabControl.classList.add('programm__tabs-btn--active')
 
-function closeModal(e) {
-    e.preventDefault()
+    }
 
-    const target = e.target
-    if ( target.closest('.modal__cancel') ||target.classList.contains('modal')){
-        modal.classList.remove('modal--opened')}
-}
+    // Аккордеон
+
+    const accordList = document.querySelectorAll('.programm__accord-list')
+
+    accordList.forEach(el => {
+        el.addEventListener('click', (e) => {
+
+            const accordList = e.currentTarget
+            const accordOpenedItem = accordList.querySelector('.accord__button--active')
+            const accordOpenedContent = accordList.querySelector('.accord__button--active .accord__content')
+
+            const accordControll = e.target.closest('.accord__button')
+
+            if (!accordControll) return
+
+            e.preventDefault()
+            const accordItem = accordControll.parentElement;
+            const accordContent = accordControll.nextElementSibling;
+
+            if (accordOpenedItem && accordItem != accordOpenedItem) {
+                accordOpenedItem.classList.remove('accord__button--active')
+                accordOpenedContent.style.maxHeight = null
+
+            }
+
+
+            accordItem.classList.toggle('accord__button--active')
+
+            if (accordItem.classList.contains('accord__button--active')) {
+                accordContent.style.maxHeight = accordContent.scrollHeight + 'px'
+            } else {
+                accordContent.style.maxHeight = null
+            }
+
+
+        })
+    })
 
 
 })()

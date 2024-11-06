4c426e6bb101a60de64aa8955390eee2d84d1fdd
salavatteach <salavat.seit.23981165@gmail.com>
2024-11-06 22:29:58 +0300

    Add files via upload

---
+++ diplomka-2/js/main.js
@@ -0,0 +1,29 @@
+(function () {
+
+    // Аккордеон навигации
+
+    const dropBtn = document.querySelector('.dropdown-btn')
+    const dropContentOpen = dropBtn.parentElement
+    const wrapper = document.querySelector('.wrapper')
+
+
+    wrapper.addEventListener('click', (e) => {
+        const target = e.target
+        console.log(target)
+        if (target && target.classList.contains('dropdown-btn')) {
+
+            dropContentOpen.classList.add('dropdown--active')
+        }
+        else if (dropContentOpen.classList.contains('dropdown--active')) {
+
+            dropContentOpen.classList.remove('dropdown--active')
+
+        }
+        else {
+            dropContentOpen.classList.remove('dropdown--active')
+
+        }
+    })
+
+
+})()
